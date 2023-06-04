3bc18bd40368c671968fbcbd749fbc83878967c1
Shrikant Anand Surwase <97949621+Shrikant-Surwase@users.noreply.github.com>
2023-06-04 21:36:12 +0530

    Update 1.Set-Matrix-Zeros.cpp

--- 1.Set-Matrix-Zeros.cpp
+++ 1.Set-Matrix-Zeros.cpp
@@ -1,11 +1,11 @@
-# 1.Set Matrix Zeros 
-## Problem Statement
+// # 1.Set Matrix Zeros 
+// ## Problem Statement
 
-Given an m x n integer matrix matrix, if an element is 0, set its entire row and column to 0's.
-You must do it in place.
+//Given an m x n integer matrix matrix, if an element is 0, set its entire row and column to 0's.
+//You must do it in place.
 
-Input: matrix = [[1,1,1],[1,0,1],[1,1,1]]
-Output: [[1,0,1],[0,0,0],[1,0,1]]
+//Input: matrix = [[1,1,1],[1,0,1],[1,1,1]]
+//Output: [[1,0,1],[0,0,0],[1,0,1]]
 
 #include <bits/stdc++.h>
 
